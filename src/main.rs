03bf1435132e00e0a698dbe06d85f51f960493c2
cryscan <cryscan@umich.edu>
2025-01-21 02:24:38 +0800

    Add cards.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Card {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
